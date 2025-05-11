2eb337f9e5fc532439ac0925ee69efda11dd13f1
hempipat <hempipat@innovasystems.co.th>
2025-05-11 18:19:05 +0700

    I just need to do and test, what I need

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,58 +3,64 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- {
+  --   'nvimtools/none-ls.nvim',
+  --   dependencies = {
+  --     'nvimtools/none-ls-extras.nvim',
+  --     'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
+  --   },
+  --   config = function()
+  --     local null_ls = require 'null-ls'
+  --     local formatting = null_ls.builtins.formatting -- to setup formatters
+  --     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+  --
+  --     -- list of formatters & linters for mason to install
+  --     require('mason-null-ls').setup {
+  --       ensure_installed = {
+  --         'prettier', -- ts/js formatter
+  --         'shfmt',
+  --         'ruff',
+  --       },
+  --       -- auto-install configured formatters & linters (with null-ls)
+  --       automatic_installation = true,
+  --     }
+  --
+  --     local sources = {
+  --       diagnostics.checkmake,
+  --       formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
+  --       formatting.stylua,
+  --       formatting.shfmt.with { args = { '-i', '4' } },
+  --       formatting.terraform_fmt,
+  --       require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+  --       require 'none-ls.formatting.ruff_format',
+  --     }
+  --
+  --     local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+  --     null_ls.setup {
+  --       -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+  --       sources = sources,
+  --       -- you can reuse a shared lspconfig on_attach callback here
+  --       on_attach = function(client, bufnr)
+  --         if client.supports_method 'textDocument/formatting' then
+  --           vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+  --           vim.api.nvim_create_autocmd('BufWritePre', {
+  --             group = augroup,
+  --             buffer = bufnr,
+  --             callback = function()
+  --               vim.lsp.buf.format { async = false }
+  --             end,
+  --           })
+  --         end
+  --       end,
+  --     }
+  --   end,
+  -- },
+  --zen mode
   {
-    'nvimtools/none-ls.nvim',
-    dependencies = {
-      'nvimtools/none-ls-extras.nvim',
-      'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
-    },
-    config = function()
-      local null_ls = require 'null-ls'
-      local formatting = null_ls.builtins.formatting -- to setup formatters
-      local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
-      -- list of formatters & linters for mason to install
-      require('mason-null-ls').setup {
-        ensure_installed = {
-          'prettier', -- ts/js formatter
-          'shfmt',
-          'ruff',
-        },
-        -- auto-install configured formatters & linters (with null-ls)
-        automatic_installation = true,
-      }
-
-      local sources = {
-        diagnostics.checkmake,
-        formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-        formatting.stylua,
-        formatting.shfmt.with { args = { '-i', '4' } },
-        formatting.terraform_fmt,
-        require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-        require 'none-ls.formatting.ruff_format',
-      }
-
-      local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-      null_ls.setup {
-        -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
-        sources = sources,
-        -- you can reuse a shared lspconfig on_attach callback here
-        on_attach = function(client, bufnr)
-          if client.supports_method 'textDocument/formatting' then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format { async = false }
-              end,
-            })
-          end
-        end,
-      }
-    end,
+    'folke/zen-mode.nvim',
+    opts = {},
   },
+
   -- nvim-cmp
   -- {
   --   'hrsh7th/nvim-cmp',
@@ -99,21 +105,73 @@ return {
   -- end,
   -- },
   --
+
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      require('notify').setup {
+        stages = 'fade_in_slide_out',
+        timeout = 3000,
+        background_colour = '#000000',
+        icons = {
+          ERROR = '',
+          WARN = '',
+          INFO = '',
+          DEBUG = '',
+          TRACE = '✎',
+        },
+      }
+      vim.notify = require 'notify'
+    end,
+  },
   {
     'rcarriga/cmp-dap',
     dependencies = { 'hrsh7th/nvim-cmp' },
     config = function()
-      require('cmp').setup {
-        enabled = function()
-          -- return vim.api.nvim
-        end,
-      }
-
-      -- require('cmp').setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
-      --   sources = {
-      --     { name = 'dap' },
-      --   },
-      -- })
+      require('cmp').setup.filetype({ 'sql', 'plsql' }, {
+        sources = {
+          { name = 'vim-dadbod-completion' },
+          -- { name = 'buffer' },
+        },
+      })
     end,
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    config = function()
+      local home = os.getenv 'HOME'
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_save_location = home .. '/db_ui_queries'
+    end,
+  },
+
+  -- Move like vim on Tmux
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
   },
 }
